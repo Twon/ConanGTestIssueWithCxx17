1b7de270477669d9db6c8d732c90c9819dc051e9
Antony Peacock <ant.peacock@gmail.com>
2024-05-02 21:17:16 +0100

    Force linkage error via gmock string view functionality

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include <string_view>
 
+class Mock
+{
+public:
+    MOCK_METHOD(void, methodUsingCxx17Types, (std::string_view), ());
+};
+
 TEST(DemoConanPackageError, SimpleTest) {
-    EXPECT_EQ(std::string_view("Hello world!"), "Hello world");
+
+    Mock mock;
+    EXPECT_CALL(mock, methodUsingCxx17Types(std::string_view("Hello world!"))).Times(1);
+
 }
